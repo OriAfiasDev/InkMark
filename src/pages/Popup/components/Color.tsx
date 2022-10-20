140df53c124ada48f36a15f370dc5b6fe196a683
OriAfiasDev <ori@afias.net>
2022-10-20 18:14:49 +0300

    isFav to Color component

--- src/pages/Popup/components/Color.tsx
+++ src/pages/Popup/components/Color.tsx
@@ -1,18 +1,31 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { MdDone } from 'react-icons/md';
+import { FavoriteIcon } from './FavoriteIcon';
 
 interface ColorProps {
   color?: string;
+  isFavorite?: boolean;
+  toggleIsFavorite: () => void;
 }
 
-export const Color: React.FC<ColorProps> = ({ color = '#fff' }) => {
+export const Color: React.FC<ColorProps> = ({
+  color = '#fff',
+  isFavorite = false,
+  toggleIsFavorite,
+}) => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const onClick = useCallback(() => {
-    navigator.clipboard.writeText(color);
-    setIsCopied(true);
-  }, [color]);
+  const copyToClipboard = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (e.target === e.currentTarget) {
+        navigator.clipboard.writeText(color);
+        setIsCopied(true);
+      }
+    },
+    [color]
+  );
 
   useEffect(() => {
     if (isCopied) {
@@ -21,13 +34,20 @@ export const Color: React.FC<ColorProps> = ({ color = '#fff' }) => {
   }, [isCopied]);
 
   return (
-    <StyledColor color={color} onClick={onClick}>
-      <StyledIcon color={color} isCopied={isCopied} />
-    </StyledColor>
+    <>
+      <StyledColor color={color} onClick={copyToClipboard}>
+        <StyledCopiedIcon color={color} isCopied={isCopied} />
+        <FavoriteIcon
+          color={color}
+          isFavorite={isFavorite}
+          toggleFavorite={toggleIsFavorite}
+        />
+      </StyledColor>
+    </>
   );
 };
 
-const StyledIcon = styled(MdDone)<{ color: string; isCopied: boolean }>`
+const StyledCopiedIcon = styled(MdDone)<{ color: string; isCopied: boolean }>`
   color: ${({ color }) => color};
   height: 24px;
   width: 24px;
@@ -37,6 +57,7 @@ const StyledIcon = styled(MdDone)<{ color: string; isCopied: boolean }>`
 `;
 
 const StyledColor = styled.div<{ color: string }>`
+  position: relative;
   height: 45px;
   width: 60px;
   border: 1px solid #e5e6ea;
