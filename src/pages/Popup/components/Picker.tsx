b41eac36d76d4811adae09f36b99c5867b96dc44
OriAfiasDev <ori@afias.net>
2022-10-24 21:24:16 +0300

    added color picker for manual save

---
+++ src/pages/Popup/components/Picker.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from 'react';
+import { ChromePicker } from 'react-color';
+import styled from 'styled-components';
+import { StyledTitle } from './ColorSection';
+
+const saveColorToLocalStorage = (color: string) => {
+  chrome.storage.local.get('savedColors', (data) => {
+    const savedColors = data.savedColors || [];
+    savedColors.push(color);
+    chrome.storage.local.set({ savedColors });
+  });
+};
+
+export const Picker = () => {
+  const [color, setColor] = React.useState('#fff');
+
+  const saveColor = useCallback(() => {
+    saveColorToLocalStorage(color);
+    window.location.reload();
+  }, [color]);
+
+  return (
+    <div>
+      <StyledTitle>Add Color</StyledTitle>
+      <ChromePicker
+        onChange={(color) => setColor(color.hex)}
+        color={color}
+        styles={{
+          default: { picker: { width: '100%' } },
+        }}
+      />
+      <SaveButton onClick={saveColor}>Save</SaveButton>
+    </div>
+  );
+};
+
+const SaveButton = styled.button`
+  width: 100%;
+  height: 40px;
+  background-color: #ff0081;
+  color: white;
+  font-family: PP Neue Machina;
+  font-weight: 400;
+  font-size: 14px;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 4px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: all 300ms ease-in-out;
+
+  &:hover {
+    background-color: #d61877;
+  }
+`;
