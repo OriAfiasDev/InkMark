51110f688156711c5f55020590f8bc43abdf7dd1
OriAfiasDev <ori@afias.net>
2022-10-26 09:45:23 +0300

    removing seed and using only stored colors

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { getColors, resetColors } from '../../utils/syncStorage';
 import { ColorSection } from './components/ColorSection';
 import { Picker } from './components/Picker';
-import { allColors } from './seed/colors';
 import { IColor } from './types/IColor';
 
 const resetColorsFromGoogleSync = async () => {
@@ -12,11 +11,11 @@ const resetColorsFromGoogleSync = async () => {
 };
 
 const Popup = () => {
-  const [stored, setStored] = React.useState<IColor[]>([]);
+  const [colors, setColors] = React.useState<IColor[]>([]);
 
   useEffect(() => {
     getColors().then((colors) => {
-      setStored(colors);
+      setColors(colors);
     });
   }, []);
 
@@ -25,24 +24,24 @@ const Popup = () => {
       <button onClick={resetColorsFromGoogleSync}>reset</button>
       <ColorSection
         title="favorites"
-        colors={allColors.filter((color) => color.isFavorite)}
+        colors={colors.filter((color) => color.isFavorite)}
       />
-      <ColorSection
-        title="stored favorites"
-        colors={stored.filter((color) => color.isFavorite)}
-      />
-      <ColorSection title="stored" colors={stored} />
+
       <ColorSection
         title="most common"
-        colors={[...stored].sort((a, b) => b.count - a.count).slice(0, 4)}
+        colors={[...colors].sort((a, b) => b.count - a.count).slice(0, 4)}
       />
+
+      <ColorSection title="all colors" colors={colors} />
+
       <ColorSection
-        title="all solids"
-        colors={allColors.filter((color) => color.type === 'solid')}
+        title="only solids"
+        colors={colors.filter((color) => color.type === 'solid')}
       />
+
       <ColorSection
-        title="all gradients"
-        colors={allColors.filter((color) => color.type === 'gradient')}
+        title="only gradients"
+        colors={colors.filter((color) => color.type === 'gradient')}
       />
       <Picker />
     </PopupContainer>
