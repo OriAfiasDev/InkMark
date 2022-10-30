54ca8b58712d7208aa13a441957c82f8962a3827
OriAfiasDev <ori@afias.net>
2022-10-30 12:24:48 +0200

    feature/get-fill

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,9 +2,11 @@ import { addNewColor } from '../../utils/syncStorage';
 
 const childIdBackground = 'CHILD_ID_BACKGROUND';
 const childIdColor = 'CHILD_ID_COLOR';
+const childIdFill = 'CHILD_ID_FILL';
 
 const getColorsFromElement = ({ menuItemId, frameId }, tab) => {
-  if (![childIdBackground, childIdColor].includes(menuItemId)) return;
+  if (![childIdBackground, childIdColor, childIdFill].includes(menuItemId))
+    return;
   const eventId = `getClickedEl-${menuItemId}`;
 
   chrome.tabs.sendMessage(tab.id, eventId, { frameId }, addNewColor);
@@ -28,6 +30,13 @@ chrome.contextMenus.removeAll(function () {
     id: childIdColor,
     parentId: 'root',
     title: 'Save Color',
+    contexts: ['all'],
+  });
+
+  chrome.contextMenus.create({
+    id: childIdFill,
+    parentId: 'root',
+    title: 'Save Fill',
     contexts: ['all'],
   });
 
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,29 +4,30 @@ document.addEventListener('contextmenu', function (event) {
   clickedEl = event.target;
 });
 
-const findElementColor = () => {
+const findElementColor = (colorType) => {
   if (!clickedEl) return null;
 
-  const { color, background, backgroundColor, backgroundImage } =
+  const { color, background, backgroundColor, backgroundImage, fill } =
     getComputedStyle(clickedEl);
-  let bg = null;
-  if (backgroundImage.includes('linear-gradient')) {
-    bg = backgroundImage;
-  } else {
-    bg = backgroundColor || background;
-  }
 
-  return {
-    color: color || '#fff',
-    bg,
-  };
+  switch (colorType) {
+    case 'getClickedEl-CHILD_ID_COLOR':
+      return color;
+    case 'getClickedEl-CHILD_ID_BACKGROUND':
+      return backgroundImage.includes('linear-gradient')
+        ? backgroundImage
+        : backgroundColor || background;
+    case 'getClickedEl-CHILD_ID_FILL':
+      const pathEl = clickedEl.querySelector('path');
+      return pathEl ? getComputedStyle(pathEl).fill : fill;
+    default:
+      return null;
+  }
 };
 
 chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
   if (request.includes('getClickedEl')) {
-    const { color, bg } = findElementColor();
-
-    return sendResponse(request.includes('CHILD_ID_BACKGROUND') ? bg : color);
+    return sendResponse(findElementColor(request));
   }
   sendResponse(null);
 });
